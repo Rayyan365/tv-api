94678c41cf102a3c69c7993c4bf9f262606459ec
Rayyan365 <rayanabdallah09@gamil.com>
2023-08-30 12:51:33 -0700

    netflix assignment

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelector.addEventListener("click", (function)
+    const searchButton = document.(".search-box"));
+    searchButton.addEventListener('click', searchNetflix);
+  ;
+  
+  function searchNetflix() {
+    const searchValue = document.querySelector(".movies-section").value;
+    const searchResult = document.querySelector('.search-results');
+   
+    searchResult.innerHTML = 'Search' + searchValue;
+
+  }
+  //ratings 
+  document.querySelector(".ratings");
+  ratings.array.forEach(element => {
+    
+  });
